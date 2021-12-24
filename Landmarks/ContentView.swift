31ebed041ff91a6bb56a6c3261ba7d13005ea193
Zaw Moe Htike <uchihamoee@gmail.com>
2021-12-24 13:43:34 +0630

    first

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+            
+            CircleImage()
+                .offset(y: -100)
+                .padding(.bottom, -100)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .foregroundColor(.blue)
+                    .font(.title)
+            
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    
+                    Spacer()
+                    
+                    Text("California")
+                        .font(.subheadline)
+                }
+                
+                Divider()
+
+                Text("About Turtle Rock")
+                    .foregroundColor(.blue)
+                    .font(.title2)
+                
+                Text("Descriptive text goes here.")
+            }.padding()
+            
+            Spacer()
+        }
     }
 }
 
