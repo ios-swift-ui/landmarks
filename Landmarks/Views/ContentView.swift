e10a378b2e310109ef591f49b95e613694f37798
Zaw Moe Htike <uchihamoee@gmail.com>
2021-12-27 21:06:21 +0630

    complex ui

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -4,10 +4,31 @@ import SwiftUI
 struct ContentView: View {
     var hikes: [Hike] = ModelData().hikes
     
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
     var body: some View {
         //LandmarkList()
         //Badge()
-        HikeView(hike: hikes[0])
+        //HikeView(hike: hikes[0])
+        
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+            
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
     }
 }
 
